5b06da6e3933f78eeb1f3a73daee03e1df692e4c
Carlos <carlos.rojas173@inacapmail.clcarlos.rojas173@inapmail.cl$ git config --global user.email>
2024-12-15 19:03:47 -0300

    Agregar archivos de proyecto.

---
+++ RegistroEstudiantes.AppMovil/MauiProgram.cs
@@ -0,0 +1,51 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using Microsoft.Extensions.Logging;
+using RegistroEstudiantes.Modelos.Modelos;
+
+namespace RegistroEstudiantes.AppMovil
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            Registrar();
+            return builder.Build();
+        }
+
+        public static void Registrar()
+        {
+            FirebaseClient client = new FirebaseClient("https://registroestudiantes-5df1f-default-rtdb.firebaseio.com/");
+
+            var cursos = client.Child("Cursos").OnceAsync<Curso>();
+
+            if (cursos.Result.Count == 0) 
+            {
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "1ro Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "2do Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "3ro Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "4to Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "5to Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "6to Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "7mo Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "8vo Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "1ro Medio" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "2do Medio" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "3ro Medio" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "4to Medio" });
+            }
+        }
+    }
+}
---
+++ RegistroEstudiantes.AppMovil/Vistas/CrearEstudiante.xaml.cs
@@ -0,0 +1,52 @@
+using Firebase.Database;
+using Firebase.Database.Query;
+using RegistroEstudiantes.Modelos.Modelos;
+
+namespace RegistroEstudiantes.AppMovil.Vistas;
+
+public partial class CrearEstudiante : ContentPage
+{
+    FirebaseClient client = new FirebaseClient("https://registroestudiantes-5df1f-default-rtdb.firebaseio.com/");
+    public List<Curso> Cursos { get; set; }
+
+    public CrearEstudiante()
+    {
+        InitializeComponent();
+        ListarCursos();
+        BindingContext = this;
+    }
+
+    private void ListarCursos()
+    {
+        var cursos = client.Child("Cursos").OnceAsync<Curso>();
+        Cursos = cursos.Result.Select(x => x.Object).ToList();
+    }
+
+    private async void guardarButton_Clicked(object sender, EventArgs e)
+    {
+        Curso curso = cursoPicker.SelectedItem as Curso;
+
+        var estudiante = new Estudiante
+        {
+            PrimerNombre = primerNombreEntry.Text,
+            SegundoNombre = segundoNombreEntry.Text,
+            PrimerApellido = primerApellidoEntry.Text,
+            SegundoApellido = segundoApellidoEntry.Text,
+            CorreoElectronico = correoEntry.Text,
+            FechaInicio = fechaInicioPicker.Date,
+            Edad = int.Parse(edadEntry.Text),
+            Curso = curso
+        };
+
+        try
+        {
+            await client.Child("Estudiantes").PostAsync(estudiante);
+            await DisplayAlert("Éxito", $"El Estudiante {estudiante.PrimerNombre} {estudiante.PrimerApellido} fue guardado exitosamente", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error!", $"Ocurrio un error al guardar el empleado: {ex.Message}", "OK");
+        }
+    }
+}
